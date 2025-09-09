bcd8a2b7d109343e43e33c83ebdaec8982c4f0f9
LuispeFigueroa <fig24087@uvg.edu.gt>
2025-09-09 12:57:43 -0600

    Primera version tabla con pelota en el centrohay que trabajar con matrices

---
+++ Pong.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <thread>
+
+const int height = 20;
+const int width = 40;
+
+void dibujarTabla(int ballX, int ballY)
+{
+
+    std::vector<std::string> tabla(height, std::string(width, ' '));
+
+    // Dibujar los bordes
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (y == 0 || y == height - 1)
+                tabla[y][x] = '*'; // bordes superior e inferior
+            else if (x == 0 || x == width - 1)
+                tabla[y][x] = '|'; // bordes laterales
+        }
+    }
+
+    // Poner la bola en posicion incial
+    tabla[ballY][ballX] = 'o';
+
+    // Imprimir la tabla
+    for (const auto &row : tabla)
+    {
+        std::cout << row << std::endl;
+    }
+}
+int main()
+{
+    // posicion inical de la pelota (en el centro)
+    int ballX = width / 2;
+    int ballY = height / 2;
+    dibujarTabla(ballX, ballY);
+
+    // ancho y largo
+}
